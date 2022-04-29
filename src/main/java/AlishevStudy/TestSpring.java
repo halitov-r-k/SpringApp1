a3dd31ac424e9d8b6c26f9c79ef4268d5224c814
halitov-r-k <halitov.r.k@gmail.com>
2022-04-29 11:51:09 +0500

    DI-1

--- src/main/java/AlishevStudy/TestSpring.java
+++ src/main/java/AlishevStudy/TestSpring.java
@@ -8,8 +8,7 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("MusicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("MusicPlayerBean", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
